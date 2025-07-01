05a3ef7ba8e156c472625e0abefaf1667b729aab
Beatriz Araujo <anabeatriz.dev@gmail.com>
2025-07-01 23:06:02 +0000

    renaming lifetime_value column

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -22,7 +22,7 @@ with customers as (
             , customer_orders.first_order_date
             , customer_orders.most_recent_order_date
             , coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-            , coalesce(customer_orders.lifetime_value, 0)
+            , coalesce(customer_orders.lifetime_value, 0) as lifetime_value
       from  customers
       left  join customer_orders using (customer_id)
      group  by all
