560cbadae1223952b5cfdd31134fc1e661a5ac7c
Beatriz Araujo <anabeatriz.dev@gmail.com>
2025-07-01 22:13:04 +0000

    refactored dim_customers to add two staging models. Set project configs to materialize all marts as tables.

---
+++ models/staging/jaflle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,4 @@
+select  id as customer_id
+        , first_name
+        , last_name
+  from  hive_metastore.default.jaffle_shop_customers
---
+++ models/staging/jaflle_shop/stg_jaffle_shop__orders.sql
@@ -0,0 +1,5 @@
+select  id as order_id
+        , user_id as customer_id
+        , order_date
+        , status 
+  from  hive_metastore.default.jaffle_shop_orders
